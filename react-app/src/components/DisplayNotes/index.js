4add65db5790c1cd29623f46ad26680c888b4597
bryant16 <kleinb1616@gmail.com>
2021-02-04 11:21:54 -0500

    notes functioning

--- react-app/src/components/DisplayNotes/index.js
+++ react-app/src/components/DisplayNotes/index.js
@@ -6,7 +6,25 @@ const DisplayNotes = ({notes})=>{
             <h1>Abilities</h1>
             {notes.filter(note=>note.title==="Abilities").map((note)=>(
             <div>
-                <h2>{note.text}</h2>
+                <h4>{note.text}</h4>
+            </div>
+            ))}
+            <h1>Physical Description</h1>
+            {notes.filter(note=>note.title==='Physical Description').map((note)=>(
+            <div>
+                <h4>{note.text}</h4>
+            </div>
+            ))}
+            <h1>Weakness</h1>
+            {notes.filter(note=>note.title==="Weakness").map((note)=>(
+            <div>
+                <h4>{note.text}</h4>
+            </div>
+            ))}
+            <h1>Summary</h1>
+            {notes.filter(note=>note.title==="Summary").map((note)=>(
+            <div>
+                <h4>{note.text}</h4>
             </div>
             ))}
         </div>
--- react-app/src/components/Notes/index.js
+++ react-app/src/components/Notes/index.js
@@ -4,28 +4,32 @@ import {createNote} from '../../store/note';
 import {getAllNotes} from '../../store/note';
 const Notes = ({playerId})=>{
     const dispatch = useDispatch();
-    const [title, setTitle] = useState('');
+    const [title, setTitle] = useState('Abilities');
     const [note, setNote] = useState('');
     const handleClickCreate = (e)=>{
         e.preventDefault();
+        console.log(title)
         const newNote = {
             title,
             note,
             playerId
         }
         setNote('')
+        console.log(title, 'titleeee')
         dispatch(createNote(newNote))
     }
-  
+    console.log(title)
     return (
         <div>
         <h1>Notes</h1>
         <form>
         <label>Title</label>
-        <input 
-        type='text'
-        value={title}
-        onChange={(e)=> setTitle(e.target.value)}/>
+        <select onChange={(e)=> setTitle(e.target.value)}>
+        <option value={'Abilities'} >Abilities</option>
+        <option value={'Physical Description'} >Physical Description</option>
+        <option value={'Weakness'}>Weakness</option>
+        <option value={'Summary'}>Summary</option>
+        </select> 
         <label>Note</label>
         <input 
         type='text'
