695c3dad7ccbe7cdb9aa8dfa06ea2b87e2ef64bd
bryant16 <kleinb1616@gmail.com>
2021-02-05 12:54:54 -0500

    splash page

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,47 +1,50 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-
-import LogoutButton from './auth/LogoutButton';
-
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import LogoutButton from "./auth/LogoutButton";
+import "./NavBar.css";
 const NavBar = () => {
   const user = useSelector((state) => state.session.user);
   return (
     <nav>
-       {!user
-              ? (
-                <>
-                  <div>
-                    <NavLink to='/sign-up'>
-                      <button>Sign Up</button>
-                    </NavLink>
-                  </div>
-                  <div>
-                    <NavLink to='/login'>
-                      <button>Login</button>
-                    </NavLink>
-                  </div>
-                </>
-                )
-              : (
-                <>
-                  <div>
-                    <NavLink to='/'>
-                      <button>Home</button>
-                    </NavLink>
-                  </div>
-                  <div>
-                    <NavLink to='/newPlayer'>
-                      <button>New Player</button>
-                    </NavLink>
-                  </div>
-                  <div>
-                    <LogoutButton />
-                  </div>
-                </>
-                )}
+      {!user ? (
+        <div className="my_page_links">
+          <p>Bryant Klein</p>
+          <p>
+          <a href="https://github.com/Bryant16">
+            <i class="fab fa-github" />
+          </a>
+          </p>
+          <p>
+          <a href="https://www.linkedin.com/in/bryant-klein-927915134/">
+            <i class="fab fa-linkedin-in" />
+          </a>
+          </p>
+          <p>
+          <a href='mailto:kleinb1616@gmail.com'>
+              <i class='far fa-envelope' />
+            </a>
+          </p>
+        </div>
+      ) : (
+        <>
+          <div>
+            <NavLink to="/">
+              <button>Home</button>
+            </NavLink>
+          </div>
+          <div>
+            <NavLink to="/newPlayer">
+              <button>New Player</button>
+            </NavLink>
+          </div>
+          <div>
+            <LogoutButton />
+          </div>
+        </>
+      )}
     </nav>
   );
-}
+};
 
 export default NavBar;
--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
-import { Redirect } from "react-router-dom";
+import { Redirect, useHistory } from "react-router-dom";
 import { LogIn } from "../../store/session";
+import './LoginForm.css';
+import logo from './new_logo_bbs.png';
+import letters from './BBScouting writing.png';
 
 const LoginForm = ({ authenticated, setAuthenticated }) => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const {user} = useSelector(state=>state.session)
   const [errors, setErrors] = useState([]);
   const [email, setEmail] = useState("");
@@ -25,20 +29,34 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
     setPassword(e.target.value);
   };
 
+  const demoLogin = (e) => {
+    e.preventDefault();
+    dispatch(LogIn('demo@aa.io', 'password'));
+  };
+  
   if (authenticated) {
     return <Redirect to="/" />;
+  }
+  const handleSignup = (e)=>{
+    e.preventDefault();
+    history.push('/sign-up');
   }
 
   return user
   ? <Redirect to='/' />:(
-    <form onSubmit={onLogin}>
+    <div className='login_container'>
+    <div className='login_form_container'>
+      <div>
+        <img src={letters}/>
+      </div>
+    <form className='form_for_login'onSubmit={onLogin}>
       <div>
         {errors.map((error) => (
           <div>{error}</div>
         ))}
       </div>
+        {/* <label htmlFor="email">Email</label> */}
       <div>
-        <label htmlFor="email">Email</label>
         <input
           name="email"
           type="text"
@@ -47,8 +65,8 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
           onChange={updateEmail}
         />
       </div>
+        {/* <label htmlFor="password">Password</label> */}
       <div>
-        <label htmlFor="password">Password</label>
         <input
           name="password"
           type="password"
@@ -56,9 +74,14 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
           value={password}
           onChange={updatePassword}
         />
-        <button type="submit">Login</button>
       </div>
+        <button type="submit">Login</button>
+        <button onClick={handleSignup} type="submit">Sign Up</button>
+        <button onClick={demoLogin}>Demo User</button>
     </form>
+    <img src={logo}/>
+    </div>
+    </div>
   );
 };
 
