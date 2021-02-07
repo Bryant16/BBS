ea867dc46e9eced32dc811646401aa6c0aa53bd9
bryant16 <kleinb1616@gmail.com>
2021-02-07 15:45:26 -0500

    refactor player profile

---
+++ react-app/src/components/PlayerProfilePage/PlayerImage.js
@@ -0,0 +1,51 @@
+import React,{useEffect, useState} from 'react';
+import defaultUser from "./default-user.png";
+import {Image} from "@chakra-ui/react";
+
+const PlayerImage = ({playerid})=>{
+    const [imageUpload, setImage] = useState('');
+    const [playerImageUrl, setPlayerImageUrl] =useState(false);
+
+    useEffect(()=>{
+        const getProfileUrl = async()=>{
+            let response = await fetch(`/api/images/${playerid}`);
+            if(response.ok){
+                let profile_url = await response.json();
+                setPlayerImageUrl(profile_url.URL)
+            }
+        }
+        getProfileUrl()
+    },[playerImageUrl])
+
+    const updateFile = async(e)=>{
+        e.preventDefault();
+        const file = e.target.files[0];
+        if(file) setImage(file);
+    }
+    const handleSubmit = async(e)=>{
+            e.preventDefault();
+            const formData = new FormData();
+            formData.append("image", imageUpload)
+            const res = await fetch(`/api/images/${playerid}`,{method:"POST",body:formData})
+            if (res.ok){
+                const imageUpload = await res.json();
+                setPlayerImageUrl(imageUpload.URL)
+            }
+    }
+    return (
+        
+        <div className='player_profile_container_image'>
+        <div>
+        {playerImageUrl ? <Image boxSize="200px" objectFit="cover" src={playerImageUrl}/>: <img className='profile_picture' src={defaultUser}/>}
+        </div>
+        <div className='file_upload_container'>
+        <form onSubmit={handleSubmit}>
+            <input type='file' name='file' onChange={updateFile} />
+            <button type="submit" >Upload</button>
+        </form>
+        </div>
+        </div>
+    )
+}
+
+export default PlayerImage;
--- react-app/src/components/PlayerProfilePage/index.js
+++ react-app/src/components/PlayerProfilePage/index.js
@@ -3,17 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams, Link, Redirect, useHistory } from 'react-router-dom';
 import {getNonePitcherForm} from '../../store/nonPitcher';
 import {getPitcherForm} from "../../store/Pitcher";
-import defaultUser from "./default-user.png";
-import {Image} from "@chakra-ui/react";
 import './PlayerProfilePage.css';
+import NewPlayerForm from '../NewPlayerForm';
+import PlayerImage from './PlayerImage';
 
 const PlayerProfilePage = ()=>{
     const history = useHistory();
     const { user } = useSelector((state) => state.session);
     const {playerid} = useParams();
     const [playerInfo, setPlayerInfo] = useState(false);
-    const [imageUpload, setImage] = useState('');
-    const [playerImageUrl, setPlayerImageUrl] =useState(false);
+    const [edit, setEdit] = useState(false);
     const dispatch = useDispatch();
 
     useEffect(()=>{
@@ -24,54 +23,26 @@ const PlayerProfilePage = ()=>{
                 setPlayerInfo(single_player.player)
             }
         }
-        const getProfileUrl = async()=>{
-            let response = await fetch(`/api/images/${playerid}`);
-            if(response.ok){
-                let profile_url = await response.json();
-                setPlayerImageUrl(profile_url.URL)
-            }
-        }
+       
         getPlayer()
-        getProfileUrl()
+       
         dispatch(getNonePitcherForm(playerid))
         dispatch(getPitcherForm(playerid))
-        console.log('rerender')
-    },[dispatch, playerImageUrl]);
+        
+    },[dispatch]);
 
-    const updateFile = async(e)=>{
-        e.preventDefault();
-        const file = e.target.files[0];
-        if(file) setImage(file);
-    }
-    const handleSubmit = async(e)=>{
-            e.preventDefault();
-            const formData = new FormData();
-            formData.append("image", imageUpload)
-            const res = await fetch(`/api/images/${playerid}`,{method:"POST",body:formData})
-            if (res.ok){
-                const imageUpload = await res.json();
-                setPlayerImageUrl(imageUpload.URL)
-            }
-    }
-    console.log(playerImageUrl)
+   
+    
     return (
     <div className='player_profile_page'>
         {playerInfo ? (
-        <div className='player_profile_container'>
-            <div className='player_profile_container_image'>
-            <div>
-            {playerImageUrl ? <Image boxSize="200px" objectFit="cover" src={playerImageUrl}/>: <img className='profile_picture' src={defaultUser}/>}
-            </div>
-            <div className='file_upload_container'>
-            <form onSubmit={handleSubmit}>
-                <input type='file' name='file' onChange={updateFile} />
-                <button type="submit" >Upload</button>
-            </form>
-            </div>
-            </div>
+            <div className='player_profile_container'>
+             <PlayerImage playerid={playerid}/>
             <div className='player_info_container'>
             <div>
                 <Link to={`/players/${playerid}/evaluation`}>Evaluation</Link>
+                <button onClick={(e)=> setEdit(!edit)}>Edit Profile</button>
+                {edit? <NewPlayerForm />:null}
                 <h2>first: {playerInfo.first_name}</h2>
                 <h2>last: {playerInfo.last_name}</h2>
                 <h2>height: {playerInfo.height}</h2>
