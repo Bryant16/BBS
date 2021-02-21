23025a02a04cbe4337eb60016a1eb5b803ff61d8
bryant16 <kleinb1616@gmail.com>
2021-02-20 21:22:10 -0500

    player updates

--- react-app/src/components/PlayerProfilePage/PlayerInfoCard.js
+++ react-app/src/components/PlayerProfilePage/PlayerInfoCard.js
@@ -7,6 +7,8 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Modal from './Modal';
+import Pdf from '../PDF';
+
 import {FaAddressCard,FaMailchimp, FaBirthdayCake, FaBaseballBall} from 'react-icons/fa';
 const useStyles = makeStyles({
   root: {
