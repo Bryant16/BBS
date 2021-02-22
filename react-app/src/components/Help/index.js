f75b271c200da3eb5ab81f011f1311509006a51c
bryant16 <kleinb1616@gmail.com>
2021-02-22 18:24:50 -0500

    add videos to help page

--- react-app/src/components/Help/index.js
+++ react-app/src/components/Help/index.js
@@ -14,8 +14,8 @@ const Help = ()=>{
             <ReactPlayer width='24em' height='24em'   controls url="https://bbscouting.s3.amazonaws.com/createPlayer.mp4" />
             </div>
             <div>
-                <h1>Test</h1>
-            <ReactPlayer width='24em' height='24em'  controls url="https://bbscouting.s3.amazonaws.com/searchFeature.mp4" />
+                <h1>Edit a Player's Information</h1>
+            <ReactPlayer width='24em' height='24em'  controls url="https://bbscouting.s3.amazonaws.com/editplayer.mp4" />
             </div>
             <div>
                 <h1>Test</h1>
