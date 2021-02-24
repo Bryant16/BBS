666500920535e50c9eab5b9248ac42284766436a
bryant16 <kleinb1616@gmail.com>
2021-02-24 18:04:10 -0500

    updated note fetch in player profile

--- react-app/src/components/PlayerProfilePage/index.js
+++ react-app/src/components/PlayerProfilePage/index.js
@@ -16,12 +16,13 @@ import {infoPDF} from '../../store/player';
 const PlayerProfilePage = ()=>{
     const nonPitcher = useSelector(state=> state.nonPitcher);
     const pitcher = useSelector((state) => state.pitcher);
-    const notes = useSelector(state => state.notes);
+    // const notes = useSelector(state => state.notes);
+    const [notes, setNotes] = useState('')
     const {playerid} = useParams();
     const [videoUrl, setVideoUrl] =useState(false);
     const [videos, setVideos] = useState(false)
     const players = useSelector(state=> state.players);
-    const [playerImageUrl, setPlayerImageUrl] =useState(false);
+    const [playerImageUrl, setPlayerImageUrl] = useState(false);
     const dispatch = useDispatch();
 useEffect(()=>{
     const getVideos=async()=>{
@@ -31,6 +32,27 @@ useEffect(()=>{
             setVideos(videos.videos)
         }
     }
+    const getTheNotes = async()=>{
+        const res = await fetch(`/api/notes/${playerid}/`);
+        if(res.ok){
+            const notesJson = await res.json();
+            const playerNotes = {}
+        notesJson.forEach(note=>{
+            if(note.title ==='Abilities'){
+              playerNotes['Abilities'] = note.text
+            }else if(note.title === 'Weakness'){
+                playerNotes['Weakness'] = note.text
+            }else if(note.title === 'Physical Description'){
+                playerNotes['Physical Description'] = note.text
+            }else{
+                playerNotes['Summary'] = note.text
+            }
+        })
+        setNotes(playerNotes)
+            }
+         }
+        getTheNotes()
+            
     const getProfileUrl = async()=>{
         let response = await fetch(`/api/media/images/${playerid}`);
         if(response.ok){
@@ -66,8 +88,6 @@ useEffect(()=>{
             setVideoUrl(video.video_url)
         }
     }
-            // setImage(file)
-    
     }
    
     const whichEval=()=>{
