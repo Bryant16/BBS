50a02769483980d8de78b1f817b4b34c28b5069a
bryant16 <kleinb1616@gmail.com>
2021-06-03 18:09:09 -0400

    refactor

--- app/api/media_routes.py
+++ app/api/media_routes.py
@@ -9,17 +9,18 @@ import mimetypes
 import magic
 
 mime = magic.Magic(mime=True)
-s3=boto3.resource('s3',
-                aws_access_key_id=Config.aws_access_key_id,
-                aws_secret_access_key=Config.aws_secret_access_key)
+s3 = boto3.resource('s3',
+                    aws_access_key_id=Config.aws_access_key_id,
+                    aws_secret_access_key=Config.aws_secret_access_key)
 
 BUCKET_NAME = 'bbscouting'
 media_routes = Blueprint('media', __name__)
 multi_config = TransferConfig(
-                        multipart_threshold=1024 * 10,
-                        max_concurrency=10,
-                        multipart_chunksize=1024 * 5,
-                        use_threads=True)
+    multipart_threshold=1024 * 10,
+    max_concurrency=10,
+    multipart_chunksize=1024 * 5,
+    use_threads=True)
+
 
 @media_routes.route('/images/<int:id>')
 def get_player_url(id):
@@ -29,20 +30,24 @@ def get_player_url(id):
     try:
         return jsonify(last)
     except:
-        return jsonify({'imageurl':False})
+        return jsonify({'imageurl': False})
+
 
 @media_routes.route('/images/<int:id>', methods=['POST'])
 def handle_image_upload(id):
     img = request.files['image']
     if img:
         filename = secure_filename(img.filename)
-        s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=img, ContentType=img.content_type, ACL='public-read')
-        newProfilePic = Image(URL="{}{}".format('https://bbscouting.s3.amazonaws.com/',filename),player_id=id)
+        s3.Bucket(BUCKET_NAME).put_object(Key=filename, Body=img,
+                                          ContentType=img.content_type, ACL='public-read')
+        newProfilePic = Image(URL="{}{}".format(
+            'https://bbscouting.s3.amazonaws.com/', filename), player_id=id)
         db.session.add(newProfilePic)
         db.session.commit()
-        return jsonify({'newPic':True})
+        return jsonify({'newPic': True})
     else:
-        return jsonify({'error':['could not be processed at this time']})
+        return jsonify({'error': ['could not be processed at this time']})
+
 
 @media_routes.route('/videos/<int:id>', methods=['POST'])
 def handle_video_upload(id):
@@ -53,31 +58,38 @@ def handle_video_upload(id):
         if int(size) < (10 * 1024 * 1024):
             try:
                 filename = secure_filename(vid.filename)
-                s3.Bucket(BUCKET_NAME).put_object(Body=vid, Key=filename, ContentType=content, ACL='public-read')
-                newVideo = Video(content= f'https://bbscouting.s3.amazonaws.com/{filename}', content_type=content, player_id=id)
+                s3.Bucket(BUCKET_NAME).put_object(
+                    Body=vid, Key=filename, ContentType=content, ACL='public-read')
+                newVideo = Video(
+                    content=f'https://bbscouting.s3.amazonaws.com/{filename}', content_type=content, player_id=id)
                 db.session.add(newVideo)
                 db.session.commit()
-                return jsonify({'video_url':  f'https://bbscouting.s3.amazonaws.com/{filename}','type':content})
+                return jsonify({'video_url':  f'https://bbscouting.s3.amazonaws.com/{filename}', 'type': content})
             except:
-                return jsonify({'upload':False})
+                return jsonify({'upload': False})
         else:
             try:
                 filename = secure_filename(vid.filename)
-                s3.Object(BUCKET_NAME, filename).upload_fileobj(vid, ExtraArgs={'ContentType' : content, 'ACL' : 'public-read'}, Config=multi_config)
-                newVideo = Video(content= f'https://bbscouting.s3.amazonaws.com/{filename}', content_type=content, player_id=id)
+                s3.Object(BUCKET_NAME, filename).upload_fileobj(vid, ExtraArgs={
+                    'ContentType': content, 'ACL': 'public-read'}, Config=multi_config)
+                newVideo = Video(
+                    content=f'https://bbscouting.s3.amazonaws.com/{filename}', content_type=content, player_id=id)
                 db.session.add(newVideo)
                 db.session.commit()
-                return jsonify({'video_url':  f'https://bbscouting.s3.amazonaws.com/{filename}','type':content})
+                return jsonify({'video_url':  f'https://bbscouting.s3.amazonaws.com/{filename}', 'type': content})
             except:
-                return jsonify({'upload' : False})
+                return jsonify({'upload': False})
+
 
 @media_routes.route('/videos/<int:id>')
 def get_videos(id):
-    videos = Video.query.filter(Video.player_id == id).order_by(Video.id.desc()).all()
+    videos = Video.query.filter(
+        Video.player_id == id).order_by(Video.id.desc()).all()
     try:
         return jsonify({'videos': [video.to_dict() for video in videos]})
     except:
-        return jsonify({'videos':False})
+        return jsonify({'videos': False})
+
 
 @media_routes.route('/videos', methods=["DELETE"])
 def delete_content():
@@ -88,4 +100,4 @@ def delete_content():
         db.session.commit()
         return jsonify({'removed': True})
     except:
-        return jsonify({'removed':False})
+        return jsonify({'removed': False})
